f4f2200bdc6301c51d9f8900d4c95877d2644502
TjarkN <tjark.nitsche@th-owl.de>
2026-03-11 13:36:27 +0100

    realized, futureproofed and cleaned up the GraphRAG-tests

--- src/process_extraction/graphrag_test/graph_rag.py
+++ src/process_extraction/graphrag_test/graph_rag.py
@@ -6,14 +6,14 @@ import graphrag.api as api
 import pandas as pd
 from graphrag.config.load_config import load_config
 from graphrag.index.typing.pipeline_run_result import PipelineRunResult
-from typing import Dict, Any
+#from typing import Dict, Any
 
 from phoenix.client import Client
 from phoenix.experiments import run_experiment
-#from process_extraction.experimentation.construct_dataset import construct_dataset
 from process_extraction.experimentation.evaluators.correctness import correctness
 from process_extraction.init_phoenix import init_phoenix
-#from process_extraction.process_extraction_decomposition import decompose_ask_about_process
+
+from process_extraction.graphrag_test.tasks import task_basic,task_drift,task_global,task_local
 
 tracer = init_phoenix("llm-process-extraction-graphrag")
 
@@ -62,44 +62,39 @@ async def run_queries(df_qa):
     
     return df_graphrag
 
-def task(input: Dict[str, Any]):
-    user_request = input["user_request"]
-    process_data = input["process_data"]
-    final_answer, context = asyncio.run(api.local_search(
-            config=graphrag_config,
-            entities=entities,
-            communities=communities,
-            community_reports=community_reports,
-            text_units= text_unit_df,
-            relationships= relationships,
-            covariates = None,
-            community_level=2,
-            response_type="Kurze präzise Antwort auf Deutsch",
-            query=user_request
-        ))
-    return {"final_answer": final_answer, "context": context}
 
-async def experiment(dataset_name, loc_qa_dataset):
+
+async def experiment(dataset_name, loc_qa_dataset, exp_name, exp_description, task_used):
     px_client = Client()
     df_qa = pd.read_csv(loc_qa_dataset, sep =";")
     #data =  await run_queries(df_qa)
     dataset_df = pd.DataFrame(df_qa)
 
-    try:
+    # Create the dataset, that is used for the 
+
+    px_dataset = px_client.datasets.get_dataset(dataset=dataset_name)
+    if px_dataset == None:
         px_dataset = px_client.datasets.create_dataset(
             dataframe=dataset_df, name=dataset_name, input_keys=["user_request", "process_data", "ground_truth"]
         )
-    except Exception as e:
-        print(f"Error creating dataset: {e}")
+    
+    try:
         px_dataset = px_client.datasets.get_dataset(dataset=dataset_name)
-
+    except Exception as e:
+        print(f"Dataset does not yet exist: {e}")
+        print("Creating dataset.")
+        px_dataset = px_client.datasets.create_dataset(
+            dataframe=dataset_df, name=dataset_name, input_keys=["user_request", "process_data", "ground_truth"]
+        )
+        
+    
     run_experiment(
         px_dataset,
-        task,
+        task_used,
         evaluators=[correctness],
         timeout=100000000,
-        experiment_name="test-experiment",
-        experiment_description="This experiment is used to test if the phoenix run_experiment method is correctly implemented",
+        experiment_name=exp_name,
+        experiment_description=exp_description,
         concurrency=8
     )
 
@@ -111,7 +106,25 @@ def index_graph():
         status = f"error\n{workflow_result.error}" if workflow_result.error else "success"
         print(f"Workflow Name: {workflow_result.workflow}\tStatus: {status}")
 
+def run_graphrag_experiments(runs, tasks, loc_qa_dataset,dataset_name,exp_name,exp_description):
+    # Method that runs the experiment-method x (runs) times for every task-method it was given 
+    for item in tasks:
+        exp_name_full = exp_name+"-"+item.__name__
+        for i in range(runs):
+            asyncio.run(experiment(dataset_name,loc_qa_dataset,exp_name_full,exp_description,item))
+
+
 if __name__ == "__main__":
+    # Variables to change for the experiment runs
     loc_qa_dataset = 'Fragen_Antworten_Dienstreise.csv'
     dataset_name = "graphrag_evaluation"
-    asyncio.run(experiment(dataset_name,loc_qa_dataset))
+    exp_name = "qwen3-8b-40k" # old: test-experiment
+    exp_description = "This experiment is used to evaluate the GraphRAG approach for the model "+exp_name
+    task_used = task_local
+    tasks = [task_local,task_global,task_basic,task_drift]
+    runs = 5
+
+    # Run the experiments with the given variables
+    run_graphrag_experiments(runs,tasks,loc_qa_dataset,dataset_name,exp_name,exp_description)
+    
+    #asyncio.run(experiment(dataset_name,loc_qa_dataset,exp_name,exp_description,task_used))
---
+++ src/process_extraction/graphrag_test/tasks.py
@@ -0,0 +1,80 @@
+import pandas as pd
+import asyncio
+import os
+
+import graphrag.api as api
+from graphrag.config.load_config import load_config
+from typing import Dict, Any
+
+graphrag_config = load_config(os.getenv("PROJECT_DIRECTORY"))
+PROJECT_DIRECTORY = os.getenv("PROJECT_DIRECTORY")
+
+entities = pd.read_parquet(f"{PROJECT_DIRECTORY}/output/entities.parquet")
+communities = pd.read_parquet(f"{PROJECT_DIRECTORY}/output/communities.parquet")
+text_unit_df = pd.read_parquet(f"{PROJECT_DIRECTORY}/output/text_units.parquet")
+relationships = pd.read_parquet(f"{PROJECT_DIRECTORY}/output/relationships.parquet")
+community_reports = pd.read_parquet(f"{PROJECT_DIRECTORY}/output/community_reports.parquet")
+
+# Set of methods that define the four different approaches in GraphRAG as tasks for the arize-phoenix pipeline
+
+def task_local(input: Dict[str, Any]):
+    user_request = input["user_request"]
+    final_answer, context = asyncio.run(api.local_search(
+            config=graphrag_config,
+            entities=entities,
+            communities=communities,
+            community_reports=community_reports,
+            text_units= text_unit_df,
+            relationships= relationships,
+            covariates = None,
+            community_level=2,
+            response_type="Kurze präzise Antwort auf Deutsch",
+            query=user_request
+        ))
+    return {"final_answer": final_answer, "context": context}
+
+def task_global(input: Dict[str, Any]):
+    user_request = input["user_request"]
+    final_answer, context = asyncio.run(api.global_search(
+            config=graphrag_config,
+            entities=entities,
+            communities=communities,
+            community_reports=community_reports,
+            community_level=2,
+            dynamic_community_selection= True,
+            response_type="Kurze präzise Antwort auf Deutsch",
+            query=user_request
+        ))
+    return {"final_answer": final_answer, "context": context}    
+
+def task_basic(input: Dict[str, Any]):
+    user_request = input["user_request"]
+    final_answer, context = asyncio.run(api.local_search(
+            config=graphrag_config,
+            entities=entities,
+            communities=communities,
+            community_reports=community_reports,
+            text_units= text_unit_df,
+            relationships= relationships,
+            covariates = None,
+            community_level=2,
+            response_type="Kurze präzise Antwort auf Deutsch",
+            query=user_request
+        ))
+    return {"final_answer": final_answer, "context": context}
+
+def task_drift(input: Dict[str, Any]):
+    user_request = input["user_request"]
+    final_answer, context = asyncio.run(api.local_search(
+            config=graphrag_config,
+            entities=entities,
+            communities=communities,
+            community_reports=community_reports,
+            text_units= text_unit_df,
+            relationships= relationships,
+            covariates = None,
+            community_level=2,
+            response_type="Kurze präzise Antwort auf Deutsch",
+            query=user_request
+        ))
+    return {"final_answer": final_answer, "context": context}
